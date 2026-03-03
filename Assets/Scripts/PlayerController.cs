4f5c678c791581dbe950a9b5c68ae179dc285645
Matthew Kleisath <matthewkleisath@gmail.com>
2026-03-03 16:06:11 -0500

    Player Movement + First Person

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,38 +2,98 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    
-    Camera mainCamera;
-    //Rigidbody rb;
-    CharacterController controller;
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
 
-    void Start()
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    public bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
     {
-        //rb = GetComponent<Rigidbody>();
-        controller = GetComponent<CharacterController>();
-        mainCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    void Update()
+    private void Update()
     {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        grounded = Physics.Raycast(transform.position,Vector3.down, playerHeight * 0.5f + 0.1f, whatIsGround);
+        MyInput();
+        SpeedControl();
+        if(grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
 
-        if (Physics.Raycast(ray, out hit))
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
         {
-            Vector3 lookDir = hit.point - transform.position;
-            lookDir.y = 0;
-            Quaternion rotation = Quaternion.LookRotation(lookDir);
-            transform.rotation = rotation;
-        }
-        
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        
-        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+            readyToJump = false;
 
-        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
-        //rb.velocity = moveDirection * moveSpeed;
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit velocity if needed
+        if(flatVel.magnitude > moveSpeed){
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }

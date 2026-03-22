16273d0543ce17cd4b000dd241ff2dcdb7f453de
Matthew Kleisath <matthewkleisath@gmail.com>
2026-03-21 20:05:35 -0400

    Traps do damage to zombies (Need animations)

---
+++ Assets/Scripts/Traps/TrapController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+
+    public float cooldownTime; 
+    private float nextFireTime = 0f;
+
+    void OnTriggerEnter(Collider other){
+        if (Time.time > nextFireTime)
+        {
+            if (other.CompareTag("Zombie")){
+                other.GetComponent<EnemyController>().TakeDamage(3);
+            }
+            Debug.Log("Trigger Activated!");
+
+            nextFireTime = Time.time + cooldownTime;
+        }
+    }
+}
